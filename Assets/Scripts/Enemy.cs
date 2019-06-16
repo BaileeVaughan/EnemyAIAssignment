8603560c27a08b27ae37297735747af4d94a39ca
Bailee Vaughan <47824584+BaileeVaughan@users.noreply.github.com>
2019-06-16 17:32:30 +1000

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Transform target; //target of the enemy
+    public float speed; //speed of the enemy
+       
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); //find the target object
+    }
+
+    void Update()
+    {
+        //if the distance between the target and the enemy is over 3 then move towards the target
+        if (Vector3.Distance(transform.position, target.position) > 3)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        //if the distance between the target and the enemy is less than 3 then move away from the target
+        if (Vector3.Distance(transform.position, target.position) < 3)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, -speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Vector3 moveDir;
+    public CharacterController charC;
+    public float normalSpeed = 10f, temporarySpeed = 10f;
+    public bool isSprint = false;
+    public Transform start;
+
+    public GameObject winstate;
+    public GameObject danger;
+
+    void Start()
+    {
+        charC = this.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        temporarySpeed = normalSpeed;
+        isSprint = false;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            temporarySpeed = 20f;
+            normalSpeed = temporarySpeed;
+            isSprint = true;
+        }
+        else
+        {
+            normalSpeed = 10f;
+            isSprint = false;
+        }
+        moveDir = transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * normalSpeed);
+        charC.Move(moveDir * Time.deltaTime);
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "Winstate")
+        {
+            winstate.SetActive(true);
+        }
+        else
+        {
+            winstate.SetActive(false);
+        }
+    }
+}
